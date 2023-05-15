2c0c06c8b2e1022dd66d8eee45efe08e62b3ab7b
MarcinZbor <marcinzb@gazeta.pl>
2023-05-15 09:31:01 +0200

    1part3buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
 
+class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    // total: 0,
+    // positiveFeedback: 0
 
-const App = () => {
-  return (
-    <>
-    Welcome
-    </>
-  );
-};
+  };
+
+onLeaveFeedback = type => {
+  this.setState(prevState => ({
+    [type]: prevState[type] + 1
+  }))
+
+}
+// countTotalFeedback()
+// countPositiveFeedbackPercentage()
+
+
+  render() {
+    return (
+      <>
+        <h1>Please leave feedback</h1>
+        <button type="button" onClick={() => this.onLeaveFeedback('good')}>Good</button>
+        <button type="button" onClick={() => this.onLeaveFeedback('neutral')}>Neutral</button>
+        <button type="button" onClick={() => this.onLeaveFeedback('bad')}>Bad</button>
+        <h2>Statistics: </h2>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+      
+      </>
+    );
+  }
+}
 
 export default App;
